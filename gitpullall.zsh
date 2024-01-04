642228558cae7499e6b6a0bdda1391794c628c0e
Flavius Raducu <55413297+FlaviusRaducu@users.noreply.github.com>
2024-01-04 09:08:26 +0000

    added gitpullall script

---
+++ gitpullall.zsh
@@ -0,0 +1,18 @@
+#!/bin/zsh
+
+# Store the current directory to return to later
+# Assumes that $start_dir contains only git repos
+start_dir=$(pwd)
+
+# Find all git repos within the specified directory and iterate
+find "$start_dir" -type d -maxdepth 1 -mindepth 1 | while read -r dir; do
+    cd "$dir"
+
+    # Echo the repo path for clarity
+    echo "Updating repo: $dir"
+    git pull
+	echo ""
+
+	cd "$start_dir"
+done
+
