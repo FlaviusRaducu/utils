2f5f9fd99d5eca9fd435038398fe2bbe1c62a0ba
Flavius Raducu <55413297+FlaviusRaducu@users.noreply.github.com>
2024-01-06 16:15:37 +0000

    added nvim config

---
+++ configs/nvim/init.lua
@@ -0,0 +1,16 @@
+-- Enable line numbers
+vim.opt.number = true
+
+-- Set tabstop and shiftwidth
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+
+-- Set folding method to indent
+vim.opt.foldmethod = "indent"
+
+-- Enable spell checking
+vim.opt.spell = true
+vim.opt.spelllang = "en_gb"
+
+-- Set colorscheme
+vim.cmd("colorscheme torte")
