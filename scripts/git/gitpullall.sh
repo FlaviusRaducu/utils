3bf3368e2fc94a018225dab38cc70c0c11b90e89
Flavius Raducu <55413297+FlaviusRaducu@users.noreply.github.com>
2024-01-30 07:37:23 +0000

    format

--- scripts/git/gitpullall.sh
+++ scripts/git/gitpullall.sh
@@ -10,10 +10,9 @@ find "$start_dir" -type d -maxdepth 1 -mindepth 1 | while read -r dir; do
     # Echo the repo path and current branch for clarity
     echo "Updating repo: $dir"
     git pull
-    echo "Current branch:":
+    echo "Current branch:"
     git branch
 	echo ""
 
 	cd "$start_dir"
 done
-
